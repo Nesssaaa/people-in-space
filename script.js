fa101ad299f9be9e011af2fca429361e3fddbf0a
Marie Langer <98027776+marielngr@users.noreply.github.com>
2023-12-14 11:09:44 +0100

    task-6_testing

--- script.js
+++ script.js
@@ -3,3 +3,40 @@ console.clear;
 console.log("Sanity check");
 
 const peopleInSpace = document.querySelector('[data-js="people-in-space"]');
+
+async function fetchPeopleInSpace() {
+  const url = "http://api.open-notify.org/astros.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    const data = await response.json();
+    console.log(data);
+    peopleInSpace.textContent = data.number;
+  } catch (error) {
+    console.log("error.message");
+  }
+}
+
+fetchPeopleInSpace();
+
+async function fetchNames() {
+  const url = "http://api.open-notify.org/astros.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    const names = document.createElement("ul");
+
+    document.body.append(names);
+  } catch (error) {
+    console.log("error.message");
+  }
+}
